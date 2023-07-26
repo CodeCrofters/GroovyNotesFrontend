253f56795b3c5fc68ec4e66ea323ad8f42e6801b
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2023-07-26 22:45:12 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# GroovyNotes Frontend
+
+## How to use
+ - After you have cloned the app, go to terminal in the root of the project.
+ - Make sure you have NodeJS and npm installed.
+ - run `npm i`.
+ - run `npm start`.
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return (
+        <h1>Hello Notes App</h1>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
